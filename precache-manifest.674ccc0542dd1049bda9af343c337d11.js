382d506bd20b8a3da9ce162b985ab8e94b16d1ba
Rodobaldo Cupull <rcupull@gmail.com>
2020-03-03 18:10:18 -0500

    Updates

---
+++ precache-manifest.674ccc0542dd1049bda9af343c337d11.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a05176a3e1e26cf52e0969b0d4526e",
+    "url": "/moviexercise/index.html"
+  },
+  {
+    "revision": "708fcd6cf698c98d813a",
+    "url": "/moviexercise/static/css/2.77092636.chunk.css"
+  },
+  {
+    "revision": "708fcd6cf698c98d813a",
+    "url": "/moviexercise/static/js/2.d9ab1639.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/moviexercise/static/js/2.d9ab1639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277c42c0c8b57a3b8ee9",
+    "url": "/moviexercise/static/js/main.a68d5e72.chunk.js"
+  },
+  {
+    "revision": "79ae88303d08de4a277a",
+    "url": "/moviexercise/static/js/runtime-main.42273d8d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/moviexercise/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/moviexercise/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/moviexercise/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/moviexercise/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/moviexercise/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
